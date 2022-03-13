4bcbe6c5ea278530b7d0d8f548ae3c9727e53cd1
teolo <matteo.lotti3@stud.unifi.it>
2022-03-13 17:40:06 +0100

    Implementate funzioni base classe Message

--- Message.cpp
+++ Message.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "Message.h"
+#include <iostream>
+
+void Message::printMessage() {
+    std::cout << "Sender: " << this->getSender() << std::endl;
+    std::cout << "Receiver: " << this->getReceiver() << std::endl;
+    std::cout << "Text: " << this->getText() << std::endl;
+}
--- Message.h
+++ Message.h
@@ -4,9 +4,55 @@
 
 #ifndef LABPROGRAMMAZIONE_MESSAGE_H
 #define LABPROGRAMMAZIONE_MESSAGE_H
+#include "User.h"
+#include <string>
 
 
 class Message {
+public:
+    Message(const User& s, const User& r, std::string t, bool read = false) : sender(s.getName()), receiver(r.getName()), text(t), read(read){};
+
+    void printMessage();
+
+    const std::string &getSender() const {
+        return sender;
+    }
+
+    void setSender(const std::string &sender) {
+        Message::sender = sender;
+    }
+
+    const std::string &getReceiver() const {
+        return receiver;
+    }
+
+    void setReceiver(const std::string &receiver) {
+        Message::receiver = receiver;
+    }
+
+    const std::string &getText() const {
+        return text;
+    }
+
+    void setText(const std::string &text) {
+        Message::text = text;
+    }
+
+    bool isRead() const {
+        return read;
+    }
+
+    void setRead(bool read) {
+        Message::read = read;
+    }
+
+
+private:
+    std::string sender, receiver;
+    std::string text;
+    bool read;
+
+
 
 };
 
