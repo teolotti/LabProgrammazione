b1851248a509d2d5255c1d80cb5666bac6a671dd
teolo <matteo.lotti3@stud.unifi.it>
2022-03-28 19:35:32 +0200

    Aggiunte classi astratte observer e subject

---
+++ Observer.h
@@ -0,0 +1,17 @@
+//
+// Created by teolo on 28/03/2022.
+//
+
+#ifndef LABPROGRAMMAZIONE_OBSERVER_H
+#define LABPROGRAMMAZIONE_OBSERVER_H
+
+class Observer {
+public:
+    virtual ~Observer() {};
+
+    virtual void update() = 0;
+
+    virtual void attach() = 0;
+    virtual void detach() = 0;
+};
+#endif //LABPROGRAMMAZIONE_OBSERVER_H
---
+++ Subject.h
@@ -0,0 +1,18 @@
+//
+// Created by teolo on 28/03/2022.
+//
+
+#ifndef LABPROGRAMMAZIONE_SUBJECT_H
+#define LABPROGRAMMAZIONE_SUBJECT_H
+#include <memory>
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual ~Subject() {};
+
+    virtual void subscribe(std::shared_ptr<Observer> o) = 0;
+    virtual void unsubscribe(std::shared_ptr<Observer> o) = 0;
+    virtual void notify() = 0;
+};
+#endif //LABPROGRAMMAZIONE_SUBJECT_H
