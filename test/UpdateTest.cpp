6244c7337f1813b9b0ee852579d529c10c82cd67
teolo <matteo.lotti3@stud.unifi.it>
2022-06-11 09:03:51 +0200

    Update Test

--- test/UpdateTest.cpp
+++ test/UpdateTest.cpp
@@ -6,14 +6,12 @@
 #include <memory>
 
 TEST(UpdateTest, getter){
-    std::cout << "   " << std::endl;
     User a("alice");
     User b("bob");
     Chat* c = new Chat(a, b);
     std::shared_ptr<Chat> ptr (c);
     Message m2(b, a, "Sì, ci sono");
     auto ptr1 = std::make_unique<UpdateTest>(ptr);
-    std::cout << ptr1->getCount() << std::endl;
     ptr1->attach();
     ASSERT_EQ(ptr1->getCount(), 0);
     ptr->addMessage(m2);
